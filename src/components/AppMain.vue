19b680f17fe030a48c0dc77022a4140c3d5f37da
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-15 10:36:20 -0500

    [add] main top section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,27 @@
 <script>
 export default {
-  name: 'AppMain'
-}
+  name: "AppMain",
+};
 </script>
 
 <template>
-  <p>M</p>
+  <div class="main">
+    <div class="top">
+      <div class="container">
+        <h1>Content goes here</h1>
+      </div>
+    </div>
+    <div class="button"></div>
+  </div>
 </template>
 
-
-<style>
-
+<style lang="scss" scoped>
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
+.top {
+  @include centerFlex;
+  background-color: #1c1c1c;
+  height: 100px;
+  font-size: 0.6rem;
+}
 </style>
