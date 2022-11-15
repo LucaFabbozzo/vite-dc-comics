807a3015936a8a80dc5285c555fbc0d1780a4934
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-15 12:55:24 -0500

    working on footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,8 +7,67 @@ export default {
 <template>
   <footer>
     <div class="top">
-      <div class="container"></div>
+      <div class="container tp">
+        <div class="left">
+          <div class="lft">
+            <h3>Dc comics</h3>
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+            <h3>Shop</h3>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <div class="ctr">
+            <h3>Dc comics</h3>
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+            <h3>Shop</h3>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+          <div class="rgt">
+            <h3>Dc comics</h3>
+            <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+            <h3>Shop</h3>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="right">
+          <img src="/dc-logo-bg.png" alt="logo-footer" />
+        </div>
+      </div>
     </div>
+
     <div class="bottom">
       <div class="container icons">
         <div class="right">
@@ -37,6 +96,33 @@ footer {
   .top {
     height: 275px;
     background-image: url("../assets/img/footer-bg.jpg");
+    overflow: hidden;
+    .tp {
+      height: 100%;
+      @include betweenFlex;
+      .left {
+        @include startFlex;
+        h3 {
+          text-transform: uppercase;
+          font-size: 0.9rem;
+        }
+        ul {
+          li {
+            list-style: none;
+          }
+          a {
+            text-decoration: none;
+            font-size: 0.6rem;
+          }
+        }
+      }
+      .right {
+        img {
+          transform: translateY(-1%);
+          width: 95%;
+        }
+      }
+    }
   }
   .bottom {
     height: 80px;
